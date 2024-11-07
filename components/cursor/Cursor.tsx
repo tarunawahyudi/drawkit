3f718cf32818a2b664dc997f558df5080e0a3e1d
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2024-11-08 00:50:03 +0700

    fixing cursor transform

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -10,7 +10,7 @@ type CursorProps = {
 
 const Cursor = ({ color, x, y, message }: CursorProps) => {
   return (
-    <div className="pointer-events-none absolute top-0 left-0" style={{ transform: `translateX(${x}px) translateX(${y}px)` }}>
+    <div className="pointer-events-none absolute top-0 left-0" style={{ transform: `translateX(${x}px) translateY(${y}px)` }}>
       <CursorSVG color={color} />
     </div>
   );
